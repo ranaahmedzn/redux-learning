313051d1790be99cf302d429619bfeba3c2627eb
Rana Ahmed <ranasheikhsbd@gmail.com>
2026-08-11 13:05:07 +0600

    feat: implement user registration with Firebase and update user profile

--- task-master/src/pages/Signup.jsx
+++ task-master/src/pages/Signup.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import loginImage from '../assets/image/login.svg';
 import { useForm, useWatch } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { createUser } from '../redux/features/user/UserSlice';
 
 const Signup = () => {
   const { handleSubmit, register, control } = useForm();
@@ -9,6 +11,8 @@ const Signup = () => {
   const confirmPassword = useWatch({ control, name: 'confirmPassword' });
   const navigate = useNavigate();
   const [disabled, setDisabled] = useState(true);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (
@@ -26,7 +30,11 @@ const Signup = () => {
 
   const onSubmit = ({ name, email, password }) => {
     // Email Password signup
-    console.log(name, email, password);
+    dispatch(createUser({
+      name: name, 
+      email: email, 
+      password: password
+    }))
   };
 
   const handleGoogleLogin = () => {
--- task-master/src/redux/features/user/UserSlice.js
+++ task-master/src/redux/features/user/UserSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import auth from '../../../utils/firebase.config';
 
 const initialState = {
     name: '',
@@ -11,15 +12,44 @@ const initialState = {
 
 export const createUser = createAsyncThunk(
     'userSlice/createUser',
-    async () => {
-        createUserWithEmailAndPassword()
+    async ({ name, email, password }) => {
+        const data = await createUserWithEmailAndPassword(auth, email, password )
+        await updateProfile(auth.currentUser, {
+            displayName: name
+        })
+        console.log(data);
+        return {
+            name: data.user.displayName,
+            email: data.user.email
+        };
     }
 )
 
 const userSlice = createSlice({
     name: 'userSlice',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(createUser.pending, (state) => {
+            state.isLoading = true
+            state.name = ''
+            state.email = ''
+            state.isError = false
+            state.error = ''
+        }).addCase(createUser.fulfilled, (state, { payload }) => {
+            state.isLoading = false
+            state.name = payload.name
+            state.email = payload.email
+            state.isError = false
+            state.error = ''
+        }).addCase(createUser.rejected, (state, { error }) => {
+            state.isLoading = false
+            state.name = ''
+            state.email = ''
+            state.isError = true
+            state.error = error.message
+        })
+    }
 })
 
 export default userSlice.reducer;
